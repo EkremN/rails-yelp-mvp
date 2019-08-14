fb31b1c3043633750f06ca6925fbbe7708bdbc06
Ekrem Nurovic <ekrem.nurovic@gmail.com>
2019-08-14 15:58:06 +0100

    added seed

--- app/controllers/restaurants_controller.rb
+++ app/controllers/restaurants_controller.rb
@@ -1,2 +1,15 @@
 class RestaurantsController < ApplicationController
+  def index
+    @restaurants = Restaurant.all
+  end
+
+  def new
+  end
+
+  def show
+  end
+
+  def create
+  end
+
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,26 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+
+restaurants_attributes = [
+  {
+    name:         'Dishoom',
+    address:      '7 Boundary St, London E2 7JE',
+    category:     'chinese',
+    phone_number:  '32114 5234'
+  },
+  {
+    name:         'Pizza East',
+    address:      '56A Shoreditch High St, London E1 6PQ',
+    category:     'japanese',
+    phone_number:  '7674 43562'
+  },
+  {
+    name:         'Saz',
+    address:      '9 Liverpool St, London E2 7JE',
+    category:     'french',
+    phone_number:  '32334 5243'
+  }
+]
+Restaurant.create!(restaurants_attributes)
